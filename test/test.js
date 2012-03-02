bb8b1aaea63659c3d1cdc09d4afb2ac0219d267e
Viktar Vauchkevich <victorenator@gmail.com>
2012-03-02 09:36:10 +0300

    test type param

--- test/test.js
+++ test/test.js
@@ -5,13 +5,18 @@ var vows = require('vows');
 
 var parser = require('../main.js');
 
+function parse(str) {
+    return function() {
+        return parser.parse(str);
+    };
+}
+
 vows.describe('HTTP Links Parser')
 .addBatch({
-    'when parse simple link': {
-        topic: function() {
-            return parser.parse('<http://example.com/TheBook/chapter2>');
-        },
+    'when parse a link': {
+        topic: parse('<http://example.com/TheBook/chapter2>'),
         'is correct': function(links) {
+            assert.isArray(links);
             assert.deepEqual(links, [
                 {
                     href: 'http://example.com/TheBook/chapter2'
@@ -19,11 +24,10 @@ vows.describe('HTTP Links Parser')
             ]);
         }
     },
-    'when parse single link': {
-        topic: function() {
-            return parser.parse('<http://example.com/TheBook/chapter2>; rel="previous"; title="previous chapter"');
-        },
+    'when parse a link with params': {
+        topic: parse('<http://example.com/TheBook/chapter2>; rel="previous"; title="previous chapter"'),
         'is correct': function(links) {
+            assert.isArray(links);
             assert.deepEqual(links, [
                 {
                     href: 'http://example.com/TheBook/chapter2',
@@ -33,13 +37,32 @@ vows.describe('HTTP Links Parser')
             ]);
         }
     },
-    
-    'when parse more links': {
-        topic: function() {
-            return parser.parse('</TheBook/chapter2>; rel="previous"; title*=UTF-8\'de\'letztes%20Kapitel, ' +
-                '</TheBook/chapter4>; rel="next"; title*=UTF-8\'de\'n%c3%a4chstes%20Kapitel');
-        },
+    'when parse a relative link': {
+        topic: parse('</abc>'),
         'is correct': function(links) {
+            assert.isArray(links);
+            assert.deepEqual(links, [{href: '/abc'}]);
+        }
+    },
+    'when parse a link with unquoted type': {
+        topic: parse('</abc>; type=text/html'),
+        'is correct': function(links) {
+            assert.isArray(links);
+            assert.deepEqual(links, [{href: '/abc', type: 'text/html'}]);
+        }
+    },
+    'when parse a link with quoted type': {
+        topic: parse('</abc>; type="text/html"'),
+        'is correct': function(links) {
+            assert.isArray(links);
+            assert.deepEqual(links, [{href: '/abc', type: 'text/html'}]);
+        }
+    },
+    'when parse more links': {
+        topic: parse('</TheBook/chapter2>; rel="previous"; title*=UTF-8\'de\'letztes%20Kapitel, ' +
+                '</TheBook/chapter4>; rel="next"; title*=UTF-8\'de\'n%c3%a4chstes%20Kapitel'),
+        'is correct': function(links) {
+            assert.isArray(links);
             assert.deepEqual(links, [
                 {
                     href: '/TheBook/chapter2',
@@ -55,21 +78,17 @@ vows.describe('HTTP Links Parser')
         }
     },
     
-    'when parse link terminated by semicolon': {
-        topic: function() {
-            return parser.parse('<http://example.com/TheBook/chapter2>; rel="previous";');
-        },
-        'throw error': function(links) {
-            assert['throws'](links);
+    'when parse a link terminated by semicolon': {
+        topic: parse('<http://example.com/TheBook/chapter2>; rel="previous";'),
+        'throw error': function(error) {
+            assert.ifError();
         }
     },
     
-    'when parse incorrect link': {
-        topic: function() {
-            return parser.parse('<http://example.com/TheBook/chapter2');
-        },
-        'throw error': function(links) {
-            assert['throws'](links);
+    'when parse an incorrect link': {
+        topic: parse('<http://example.com/TheBook/chapter2'),
+        'throw error': function(error) {
+            assert.ifError();
         }
     }
     
